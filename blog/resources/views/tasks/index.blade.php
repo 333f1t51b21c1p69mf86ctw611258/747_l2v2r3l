9f97ce5b958deea99f3b22a290a6c524b7c9584a
333f1t51b21c1p69mf86ctw611258 <mr.nguyenth86@gmail.com>
2017-05-02 01:00:18 +0700

    Change something

--- blog/resources/views/tasks/index.blade.php
+++ blog/resources/views/tasks/index.blade.php
@@ -11,7 +11,7 @@
         <ul>
             @foreach ($tasks as $task)
             <li>
-                <a href="/tasks/{{ $task->id }}">{{ $task->body }}</a>
+                <a href="/tasks/{{ $task->id }}">Item {{ $task->id }}: {{ $task->body }}</a>
             </li>            
             @endforeach
         </ul>
