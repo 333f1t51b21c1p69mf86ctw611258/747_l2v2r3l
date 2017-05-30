3e36d60cfdfff4ad58971b71e31bf7d5c582ca9e
Tran Huu Nguyen <nguyenth@dasannetworks.com>
2017-05-30 14:47:20 +0700

    Create Favorite vue component

---
+++ blog/app/Http/Controllers/PostsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use Illuminate\Support\Facedes\Auth;
+
+class PostsController extends Controller
+{
+    
+    /**
+     * Display a paginated list of posts.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $posts = Post::paginate(5);
+
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Favorite a particular post
+     *
+     * @param  Post $post
+     * @return Response
+     */
+    public function favoritePost(Post $post)
+    {
+        Auth::user()->favorites()->attach($post->id);
+
+        return back();
+    }
+
+    /**
+     * Unfavorite a particular post
+     *
+     * @param  Post $post
+     * @return Response
+     */
+    public function unFavoritePost(Post $post)
+    {
+        Auth::user()->favorites()->detach($post->id);
+
+        return back();
+    }
+}
